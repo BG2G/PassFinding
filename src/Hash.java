7bac64ae8cc24927a7ee1d4886d5029edc777f93
PrPopCorn <nicolas.palpacuer@live.fr>
2015-03-29 20:23:10 -0400

    Hashing problem

--- src/Hash.java
+++ src/Hash.java
@@ -2,44 +2,33 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 
 public class Hash {
 	
 	
 	
-	private MessageDigest md;
+
 	private String hashAlgorithm = "MD5";
 	
 	public Hash(String algo){
 		this.hashAlgorithm = algo;
-		try {
-			this.md = MessageDigest.getInstance(algo);
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 	}
 	
 	public Hash(){
-		try {
-			this.md = MessageDigest.getInstance(hashAlgorithm);
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 	
 	public byte[] hashing (String password){
-		this.md.reset();
-		try {
-			this.md.update(password.getBytes("UTF-8"));
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if(this.hashAlgorithm.equals("MD5")){
+			return DigestUtils.md5(password);
+		}else if(this.hashAlgorithm.equals("SHA1")){
+			return DigestUtils.sha1(password);
 		}
-		return md.digest();
-	}
+		return DigestUtils.md5(password);
+	} 
 	
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		
-		
+		//Hash hash = new Hash();
+	
+		System.out.println(DigestUtils.md5("abc".getBytes(StandardCharsets.UTF_8)));
+		System.out.println(DigestUtils.md5("sedrfghjkrewh".getBytes(StandardCharsets.UTF_8)));
+		System.out.println(DigestUtils.md5("abc".getBytes(StandardCharsets.UTF_8)));
+		System.out.println(DigestUtils.md5("abc".getBytes(StandardCharsets.UTF_8)));
+		/*
 		try {
 			Test.test1();
 		} catch (InterruptedException e) {
@@ -12,7 +22,10 @@ public class Main {
 			e.printStackTrace();
 		}
 		
-
+		while(true);
+		*/
+		
+		//Test.testHashfileGeneration();
 	}
 
 }
